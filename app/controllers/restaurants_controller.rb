39a8457e9b18f19c717f3d8fbfcefdad1e541e6f
juliette magin <juliette.magin@gmail.com>
2023-08-16 17:11:25 +0200

    corrigé la faute fiou

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -13,7 +13,7 @@ class RestaurantsController < ApplicationController
 
   def create
     @restaurant = Restaurant.new(restaurant_params)
-    if @restaurant.save!
+    if @restaurant.save
       redirect_to restaurant_path(@restaurant), notice: "Restaurant was successfully created."
     else
       render :new, status: :unprocessable_entity
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,7 +8,7 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
-    if @review.save!
+    if @review.save
       redirect_to restaurant_path(@restaurant), notice: 'Review was successfully created.'
     else
       render :new, status: :unprocessable_entity
